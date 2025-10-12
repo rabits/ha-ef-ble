c63d876e4d8ddf37a3483f8a2ae96c4dba186c2c
hypercorrection <236609634+hypercorrection@users.noreply.github.com>
2025-10-12 16:15:31 +0100

    Add Delta 2 as a device type

--- custom_components/ef_ble/eflib/devices/delta2.py
+++ custom_components/ef_ble/eflib/devices/delta2.py
@@ -6,10 +6,10 @@ from ..packet import Packet
 
 
 class Device(DeviceBase):
-    """Wave 2"""
+    """Delta 2"""
 
-    SN_PREFIX = b"KT21"
-    NAME_PREFIX = "EF-KT2"
+    SN_PREFIX = b"R331"
+    NAME_PREFIX = "EF-R33"
 
     @classmethod
     def check(cls, sn):
@@ -21,12 +21,12 @@ class Device(DeviceBase):
         super().__init__(ble_dev, adv_data, sn)
 
     async def packet_parse(self, data: bytes) -> Packet:
-        # Wave 2 uses V2 protocol with XOR encoding based on sequence number
+        # Delta 2 uses V2 protocol with XOR encoding based on sequence number
         return Packet.fromBytes(data, is_xor=True)
 
     async def data_parse(self, packet: Packet) -> bool:
         """Processing the incoming notifications from the device"""
-        # TODO: Wave 2 protobuf messages need to be identified
+        # TODO: Delta 2 protobuf messages need to be identified
         # For now, just log unknown packets
         self._logger.debug(
             "%s: %s: Unknown packet: src=0x%02X, cmdSet=0x%02X, cmdId=0x%02X, payload=%s",
